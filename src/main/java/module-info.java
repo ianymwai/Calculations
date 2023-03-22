82123b0e54556406f43d91a48630e01af2cc4aeb
ianymwai <iamianmwai@gmail.com>
2023-03-22 21:47:46 +0300

    hello

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.calculations {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires kotlin.stdlib;
-
-
-    opens com.example.calculations to javafx.fxml;
-    exports com.example.calculations;
-}
