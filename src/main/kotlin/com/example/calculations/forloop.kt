6ba9f6b2abdd495e6f96d0d292ce0364187a4177
ianymwai <iamianmwai@gmail.com>
2023-03-24 19:53:05 +0300

    kotlin for example

---
+++ src/main/kotlin/com/example/calculations/forloop.kt
@@ -0,0 +1,15 @@
+package com.example.calculations
+
+fun main() {
+
+    var numbers = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+    for (num in numbers) {
+        print("$num\t")
+    }
+
+    for (a in 1..5) {
+        println()
+        print("Number: $a\t")
+    }
+}
